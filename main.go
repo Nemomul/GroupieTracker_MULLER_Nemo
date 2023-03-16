c7ae1eb9907d2bd35e5999676e8de2eee9e410a9
Nemo <nemo.muller@ynov.com>
2023-03-16 23:47:58 +0100

    Troisième push
    Petit avancements

--- main.go
+++ main.go
@@ -35,6 +35,9 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		id := r.FormValue("id")
+		if id == "" {
+			id = "1"
+		}
 		character, err := getCharacter(id)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -54,10 +57,31 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		characters, err := getAllCharacters()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tmpl, err := template.ParseFiles("static/html/test.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, characters)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
 	http.ListenAndServe(":80", nil)
 }
 
 func getCharacter(id string) (*Character, error) {
+
 	url := fmt.Sprintf("https://rickandmortyapi.com/api/character/%s", id)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -74,49 +98,42 @@ func getCharacter(id string) (*Character, error) {
 	return &character, nil
 }
 
-// func main() {
-// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		id := r.FormValue("id")
-// 		character, err := getCharacter(id)
-// 		if err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		tmpl, err := template.ParseFiles("index.html")
-// 		if err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		err = tmpl.Execute(w, character)
-// 		if err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-// 	})
-
-// 	http.ListenAndServe(":80", nil)
-// }
-
-// func getCharacter(id string) (*Character, error) {
-// 	url := fmt.Sprintf("https://rickandmortyapi.com/api/character/%s", id)
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	var character Character
-// 	err = json.NewDecoder(resp.Body).Decode(&character)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return &character, nil
-// }
-
 /*
 func getCharacter(id int) (*Character, error) {
 	url := fmt.Sprintf("https://rickandmortyapi.com/api/character/%d", id)
 }*/
+
+func getAllCharacters() ([]Character, error) {
+	url := "https://rickandmortyapi.com/api/character"
+	var characters []Character
+
+	for {
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var pageCharacters struct {
+			Info struct {
+				Next string `json:"next"`
+			} `json:"info"`
+			Results []Character `json:"results"`
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(&pageCharacters)
+		if err != nil {
+			return nil, err
+		}
+
+		characters = append(characters, pageCharacters.Results...)
+
+		if pageCharacters.Info.Next == "" {
+			break
+		}
+
+		url = pageCharacters.Info.Next
+	}
+
+	return characters, nil
+}
